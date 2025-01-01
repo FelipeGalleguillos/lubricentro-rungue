764cd0089bab5ed61708117059a89fbde0ab51ae
Felipe Galleguillos <f.i.galleguillos@gmail.com>
2025-01-01 16:14:44 -0300

    primer commit

---
+++ src/main/java/com/lubricentro/repuestosrungue/RepuestosrungueApplication.java
@@ -0,0 +1,13 @@
+package com.lubricentro.repuestosrungue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepuestosrungueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepuestosrungueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lubricentro/repuestosrungue/RepuestosrungueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lubricentro.repuestosrungue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepuestosrungueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
